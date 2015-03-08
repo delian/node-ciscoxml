76269dd1d8eaf8e91c50d4246e1e5ef6c56910a3
Delian Delchev <delian.delchev@gmail.com>
2015-03-08 20:08:40 +0000

    Now we can complete the authentication and as well we can override the regex that define when we are asked for username and when we are asked for password
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- ciscoxml.js
+++ ciscoxml.js
@@ -3,26 +3,53 @@
  */
 
 var net = require('net');
-var debug = require('debug');
+var debug = require('debug')('ciscoxml');
+var util = require('util');
+var clone = require('clone');
 
 function Session(config) {
     if (!(this instanceof Session)) return new Session(config);
 
-    this.config = config;
-    this.client = new net.Socket();
-    this.connected = false;
-    this.authenticated = false;
-    this.buffer = "";
+    var me = this;
+
+    me.config = {
+        host: '127.0.0.1',
+        port: '38751',
+        userPromptRegex: /(Username|Login)\:\s*/i,
+        passPromptRegex: /Password\:\s*/i,
+        xmlPromptRegex: /XML\>\s*/,
+        authFailRegex: /Authentication Failed/i,
+        username: 'guest',
+        password: 'guest'
+    };
+    if (typeof config == 'object') util._extend(me.config,config);
+    me.client = new net.Socket();
+    me.connected = false;
+    me.authenticated = false;
+    me.buffer = "";
+
+    me.client.on('end',function() {
+        debug('Session ended');
+        me.connected = false;
+        me.authenticated = false;
+    });
 
     return this;
 }
 
-Session.prototype.connect = function(config) {
+Session.prototype.connect = function(config,cb) {
     var me = this;
+    if (typeof config == 'object') util._extend(me.config,config);
+    debug('Trying to connect to %s:%s',me.config.host, me.config.port);
+    me.client.on('error',function(err) {
+        debug('Connect error %s',err);
+        if (typeof cb == 'function') cb(err);
+    });
     this.client.connect(
-        config.port || this.config.port || 38751,
-        config.host || this.config.host || '127.0.0.1',
+        me.config.port,
+        me.config.host,
         function() {
+            debug('Connected to %s:%s',me.config.host,me.config.port);
             me.connected = true;
             me.authenticated = false;
             me.client.on('data',function(data) {
@@ -33,17 +60,28 @@ Session.prototype.connect = function(config) {
                     // Check for Login: or Username: and send the username
                     // Check for Password: and send password
                     // Check for XML and terminate the authentication
-                    if (me.buffer.match(/XML\>\s*/)) {
+                    debug('AUTH: >>> %s',me.buffer);
+                    if (me.buffer.match(me.config.xmlPromptRegex)) {
                         me.authenticated = true;
-                        me.buffer = ""; // Should I do that?
+                        me.buffer = "";
+                        debug('AUTH: ---- Authentication successful!');
+                        if (typeof cb == 'function') return cb(null,me);
+                        return; // Successful completion
                     } else {
-                        if (me.buffer.match(/(Username|Login)\:\s*/i)) {
-                            me.client.write((config.username || this.config.username)+'\n');
+                        if (me.buffer.match(me.config.userPromptRegex)) {
+                            me.client.write(me.config.username+'\r\n');
                             me.buffer = "";
+                            debug('AUTH: <<< %s',me.config.username);
                         }
-                        if (me.buffer.match(/Password\:\s*/i)) {
-                            me.client.write((config.password || this.config.password)+'\n');
+                        if (me.buffer.match(me.config.passPromptRegex)) {
+                            me.client.write(me.config.password+'\r\n');
                             me.buffer = "";
+                            debug('AUTH: <<< %s',me.config.password);
+                        }
+                        if (me.buffer.match(me.config.authFailRegex)) {
+                            debug('AUTH: !!!! Authentication failed!');
+                            if (typeof cb == 'function') cb(new Error('Authentication Failed'));
+                            return me.client.end(); // Fail
                         }
                     }
                 }
